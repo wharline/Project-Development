b3f6d4709848ab6c4f1dbbae5a85cb9130f59256
Derek <dbjerke@student.alamo.edu>
2016-03-03 21:38:43 -0600

    Update Unit.h

--- Bubblegum_Paintbrawl/Bubblegum_Paintbrawl/Unit.h
+++ Bubblegum_Paintbrawl/Bubblegum_Paintbrawl/Unit.h
@@ -5,17 +5,24 @@
 
 class Unit
 {
-	enum Direction{ North, South, East, West};
+	
 public:
+	enum Direction{ North, South, East, West};
+	enum ClassType{Bruiser, Ranger, Balanced};
+
 	Unit();
 	~Unit();
-	void initUnit();
-	void initUnit(int x, int y, int allegiance);
+
+	void initUnit(ClassType unitClass, int x, int y);
+	void initBruiser(int x, int y);
+	void initRanger(int x, int y);
+	void initBalanced(int x, int y);
+
 	void move(Direction dir);
-	void attack(Unit enemyUnit);
-	bool select();
+	void attack(Unit* enemyUnit);
 	void takeDamage(int damage);
-	int checkAllegiance(Unit currentUnit);
+	int checkAllegiance(Unit& currentUnit);
+	int getHealth(){return currentHealth;}
 	void lockUnit();
 
 private:
