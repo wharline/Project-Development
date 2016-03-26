5575aa266518d0cfaf1ee5c187144f4e96088ff7
Derek <dbjerke@student.alamo.edu>
2016-03-25 21:41:43 -0500

    Added function for searching for reachable tiles

--- Bubblegum_Paintbrawl/Bubblegum_Paintbrawl/grid.cpp
+++ Bubblegum_Paintbrawl/Bubblegum_Paintbrawl/grid.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "grid.h"
-
+#include "assert.h"
 Grid::Grid ()
 {
 	m_grid = NULL;
@@ -52,6 +52,45 @@ void Grid::removeTrap ()
 {
 }
 
+void Grid::checkReachableTiles(int x, int y, int remainingMoves)
+{
+	// Ensure that the provided coordinates do not breach the bounds of the Grid
+	if( ( x < 0 || y < 0 ) || (x > m_colNum || y > m_rowNum ))
+	{
+		assert(false);
+	}
+	// Create point to store tile locations with.
+	POINT currentPoint;
+	currentPoint.x = x;
+	currentPoint.y = y;
+
+	// Make referenc to current Tile
+	Tile& currentTile = m_grid[x][y];
+	
+	if( (currentTile.isEmpty()) && (currentTile.isBlocked() == false) ) reachableTiles.push_back(currentPoint);
+
+	// Check Left
+	if ( ( x > 0) && (remainingMoves > 0) )
+	{
+		checkReachableTiles(x - 1, y, remainingMoves - 1);
+	}
+	// Check Top
+	if ( ( y > 0) && (remainingMoves > 0) )
+	{
+		checkReachableTiles(y - 1, y, remainingMoves - 1);
+	}
+	// Check Right
+	if ( ( x < ( m_colNum - 1 ) ) && ( remainingMoves > 0 ) )
+	{
+		checkReachableTiles(x + 1, y, remainingMoves - 1);
+	}
+	// Check Bottom
+	if ( ( y < ( m_rowNum - 1 ) ) && ( remainingMoves > 0 ) )
+	{
+		checkReachableTiles(y + 1, y, remainingMoves - 1);
+	}
+
+}
 
 void Grid::moveToSpace ( int x, int y )
 {
