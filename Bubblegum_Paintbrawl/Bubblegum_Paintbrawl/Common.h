7bca3a9c5e492b9334c3fd094e4b965646c68619
wharline <wharline@student.alamo.edu>
2016-04-07 19:13:05 -0500

    Fixed conflicts from merging visuals branch with master.

---
+++ Bubblegum_Paintbrawl/Bubblegum_Paintbrawl/Common.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// Common header files to bew used throughout project
+
+// header files
+#define WIN32_EXTRA_LEAN
+#define DIRECTINPUT_VERSION 0x0800
+#include <windows.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+#include <dinput.h>
+#include <xinput.h>
+#include <ctime>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// libraries
+#pragma comment( lib, "winmm.lib" )
+#pragma comment( lib, "user32.lib" )
+#pragma comment( lib, "gdi32.lib" )
+#pragma comment( lib, "dxguid.lib" )
+#pragma comment( lib, "d3d9.lib" )
+#pragma comment( lib, "d3dx9.lib" )
+#pragma comment( lib, "dinput8.lib" )
+#pragma comment( lib, "xinput.lib" )
